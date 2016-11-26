7640f9f35edaa20d28d3537abb56595da04cb0c3
Ivan Vynogradov <vaniavinogradov@rambler.ru>
2016-11-26 15:11:17 +0200

    Client implements

--- src/main/java/CalculatorRestService.java
+++ src/main/java/CalculatorRestService.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
 /**
  * Created by ipvinner on 26.11.2016.
